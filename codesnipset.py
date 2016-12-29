e7051173cd661800dcbf88bc9df65b7f8cc01679
iMac <imac@iMacui-iMac.local>
2016-12-30 02:11:28 +0900

    crawler add

--- codesnipset.py
+++ codesnipset.py
@@ -189,3 +189,59 @@ def records_from(excel, sheet_index=0):
 path = '마약잔량.xls'
 ret = records_from(path)
 ret = sort_record(ret, ordering=['약품명', '불출일자'])
+
+
+# 테이블, url 크롤링
+from bs4 import BeautifulSoup
+from urllib.request import *
+from urllib.parse import *
+import re
+
+class Crawler(object):
+	"""docstring for Crawler"""
+	def __init__(self, url, page_encoding='utf-8', **req_header):
+		self.url = url
+		req = Request(url)
+		for hdr, val in req_header.items():
+			req.add_header(hdr, val)
+		self.soup = BeautifulSoup(urlopen(req).read().decode(encoding=page_encoding), 'html.parser')
+
+
+	def show_html(self):
+		print(self.soup)
+		
+	def ext_links(self, regPattern, **tagAttr):
+		rex = re.compile(regPattern)
+		for tag, attr in tagAttr.items():
+			qry = '{}[{}]'.format(tag, attr)
+			links = self.soup.select(qry)
+			return [link for link in links if rex.search(link[attr])]
+
+	def ext_tables(self, *column, only_data=True):
+		spc = re.compile('\s+')
+		ret = []
+		for table in self.soup('table'):
+			if table('table'):
+				continue
+			hdr, *recs = table('tr')
+			hdr_val = [spc.sub(' ', hdr.text).strip() for hdr in hdr.select('td, th')]
+
+			if set(column) <= set(hdr_val):
+				if only_data:
+					ret+=[dict(zip(hdr_val, [spc.sub(' ',rec.text).strip() for rec in rec('td')])) for rec in recs]
+				else:
+					ret+=[dict(zip(hdr_val, [rec for rec in rec('td')])) for rec in recs]
+		return ret
+
+
+
+
+
+
+
+
+
+
+
+
+
